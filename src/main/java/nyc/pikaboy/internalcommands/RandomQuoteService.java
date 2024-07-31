eb20a4197a8b9670e278995a84d09ac479967d1c
Lucas Ortiz <lucas.ortiz@nyu.edu>
2024-07-31 19:35:50 -0400

    Modified Bean Definitions to permit randomizer bounds (remove Settings json support slowly)

--- src/main/java/nyc/pikaboy/internalcommands/RandomQuoteService.java
+++ src/main/java/nyc/pikaboy/internalcommands/RandomQuoteService.java
@@ -1,19 +1,24 @@
 package nyc.pikaboy.internalcommands;
 
+import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import nyc.pikaboy.data.CheezlQuote;
 import nyc.pikaboy.service.CheezlQuotesService;
 import nyc.pikaboy.settings.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Random;
 @Service
+@RequiredArgsConstructor
 public class RandomQuoteService {
     private CheezlQuotesService cheezlQuotesService;
     private Settings settings;
+    @Value("${cheezlbot.randomizer-bounds}")
+    private int randomizerBounds;
     @Autowired
     RandomQuoteService(CheezlQuotesService cheezlQuotesService, Settings settings){
         this.cheezlQuotesService = cheezlQuotesService;
@@ -24,7 +29,7 @@ public class RandomQuoteService {
 
         if (settings.getAllowedTextChannels().contains(event.getChannel().getId()) && !event.getAuthor().isBot()) {
             Random randnumbergen = new Random();
-            int number = randnumbergen.nextInt(0, settings.getRandomizerBounds());
+            int number = randnumbergen.nextInt(0, randomizerBounds);
             if (number == 0) {
                 Optional<CheezlQuote> retQuote = Optional.of(cheezlQuotesService.getRandomQuote());
                 retQuote.ifPresent((quote) -> {
--- src/main/java/nyc/pikaboy/listeners/SomeCustomListener.java
+++ src/main/java/nyc/pikaboy/listeners/SomeCustomListener.java
@@ -1,5 +1,6 @@
 package nyc.pikaboy.listeners;
 
+import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import nyc.pikaboy.internalcommands.HttpCheezlClient;
@@ -9,19 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SomeCustomListener extends ListenerAdapter {
 
-    RandomQuoteService randomQuoteService;
-    HttpCheezlClient httpCheezlClient;
-    @Autowired
-    SomeCustomListener(RandomQuoteService randomQuoteService, HttpCheezlClient httpCheezlClient){
-        this.randomQuoteService = randomQuoteService;
-        this.httpCheezlClient = httpCheezlClient;
-    }
+    private final RandomQuoteService randomQuoteService;
+    private final HttpCheezlClient httpCheezlClient;
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        this.httpCheezlClient.submitMessageLog(event);
         this.randomQuoteService.sendRandomizedQuote(event);
+        this.httpCheezlClient.submitMessageLog(event);
     }
 
 
